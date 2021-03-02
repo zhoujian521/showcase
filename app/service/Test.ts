23ef7577cd62b0b942c598a558c543ad754f5ef0
zhoujian <zhoujj-f@glodon.com>
2021-03-02 16:21:44 +0800

    style: 格式化代码

--- app/service/Test.ts
+++ app/service/Test.ts
@@ -1,10 +1,9 @@
-import { Service } from 'egg';
+import { Service } from "egg";
 
 /**
  * Test Service
  */
 export default class Test extends Service {
-
   /**
    * sayHi to you
    * @param name - your name
