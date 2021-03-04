c31d07b36fd9d71c7abb470ed07954c75ddc8041
zhoujian <zhoujj-f@glodon.com>
2021-03-04 10:37:23 +0800

    feat(package.json): 添加依赖 egg-mysql

--- app.ts
+++ app.ts
@@ -8,7 +8,6 @@ export default class AppBootHook implements IBoot {
   }
 
   configWillLoad() {
-    console.log("============configWillLoad=================");
     // 此时 config 文件已经被读取并合并，但是还并未生效
     // 这是应用层修改配置的最后时机
     // 注意：此函数只支持同步调用
@@ -16,11 +15,9 @@ export default class AppBootHook implements IBoot {
 
   configDidLoad() {
     // Config, plugin files have loaded.
-    console.log("============configDidLoad=================");
   }
 
   async didLoad() {
-    console.log("============didLoad========================");
     // 所有的配置已经加载完毕
     // 可以用来加载应用自定义的文件，启动自定义的服务
     // app/extend/application.ts ==> 无法读取配置
@@ -32,18 +29,15 @@ export default class AppBootHook implements IBoot {
   }
 
   async willReady() {
-    console.log("============willReady========================");
     // 所有的插件都已启动完毕，但是应用整体还未 ready
     // 可以做一些数据初始化等操作，这些操作成功才会启动应用
   }
 
   async didReady() {
-    console.log("============didReady========================");
     // 应用已经启动完毕
   }
 
   async serverDidReady() {
-    console.log("============serverDidReady===================");
     // http / https server 已启动，开始接受外部请求
     // 此时可以从 app.server 拿到 server 的实例
   }
